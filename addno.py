18153f5a7fdc35d648056f8d53cb05e856e94be1
Meghana <115769637+meghanasiddu23@users.noreply.github.com>
2024-06-19 11:35:36 +0530

    Update addno.py

--- addno.py
+++ addno.py
@@ -1,32 +1,20 @@
-def add(a, b):
+def add(a, b,c):
     """
     Add two numbers.
 
     Parameters:
     a (int or float): The first number.
     b (int or float): The second number.
+    c (int or float): The third number.
 
     Returns:
     int or float: The sum of the two numbers.
     """
-    return a + b
+    return a + b +c
 
-def subtract(a, b):
-    """
-    Subtract the second number from the first number.
 
-    Parameters:
-    a (int or float): The number to be subtracted from.
-    b (int or float): The number to subtract.
-
-    Returns:
-    int or float: The result of the subtraction.
-    """
-    return a - b
 
 if __name__ == "__main__":
-    result_add = add(3, 5)
+    result_add = add(3, 5, 4)
     print(f"The result of addition is: {result_add}")
 
-    result_subtract = subtract(10, 2)
-    print(f"The result of subtraction is: {result_subtract}")
